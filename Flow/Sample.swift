3169d98e58f6dd8d6da26fd68ab26134916b3de7
ufukcanli <iletisim@ufukcanli.com>
2021-12-15 14:54:14 +0300

    Add flow

---
+++ Flow/Sample.swift
@@ -0,0 +1,14 @@
+//
+//  Sample.swift
+//  Flow
+//
+//  Created by Ufuk Canlı on 15.12.2021.
+//
+
+import SwiftUI
+
+struct SampleView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
